7aacbdf08356075fe97c08d23289f8b509fd21f2
ernestobone98 <ernestobone98>
2023-04-09 18:00:12 +0200

    tree structure rebuilded - Modules not working

--- datatrater.py
+++ datatrater.py
@@ -5,6 +5,6 @@ def get_airport_iata_code():
     Returns a list of airport iata codes
     '''
 
-    df = pd.read_csv('airportgraph/airports.csv')
+    df = pd.read_csv('airports.csv')
     df = df.dropna(subset=['iata_code'])
     return df['iata_code'].tolist()
