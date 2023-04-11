3b1846c15f1ce9e91b96bc537086e5fbe4cecc53
ernestobone98 <ernestobone98>
2023-04-11 12:29:41 +0200

    preprocessing data for NN application

--- api_calls.py
+++ api_calls.py
@@ -1,5 +1,6 @@
 from amadeus import Client, ResponseError
 import tokens as to
+import pandas as pd
 
 amadeus = Client(
     client_id = to.client_id,
@@ -37,8 +38,8 @@ def cheapest_date(ori, dest):
         '''
         Find cheapest dates from Madrid to Munich
         '''
-        response = amadeus.shopping.flight_dates.get(origin = ori, destination = dest, departureDate = '2023-04-09')
-        print(response.data)
+        response = amadeus.shopping.flight_dates.get(origin = ori, destination = dest, departureDate = '2023-11-11')
+        print("cheapest date : ", pd.DataFrame(response.data))
     except ResponseError as error:
         raise error
 
@@ -55,10 +56,44 @@ def airport_routes(origin):
     except ResponseError as error:
         raise error
 
+def get_cheapest_price(origin, dest):
+    try:
+        '''
+        Find the cheapest flights from SYD to BKK
+        '''
+        response = amadeus.shopping.flight_offers_search.get(
+            originLocationCode = origin, destinationLocationCode = dest, departureDate='2023-11-11', adults=1)
+        df = pd.DataFrame(response.data)
+        # from df convert all list-like elements to columns
+        df = pd.concat([df.drop(['price'], axis=1), df['price'].apply(pd.Series)], axis=1)
+        # drop type, id and source
+        df = df.drop(['type', 'id', 'source'], axis=1)
+        # drop currency (assuming all prices are in the same currency)
+        df = df.drop(['currency'], axis=1)
+        # convert travelerPricings list-like elements to columns (travelerId, fareOption, travelerType, price)
+        df = pd.concat([df.drop(['travelerPricings'], axis=1), df['travelerPricings'].apply(pd.Series)], axis=1)
+        df = pd.concat([df.drop([0], axis=1), df[0].apply(pd.Series)], axis=1)
+        # delete repeated or useless columns
+        df = df.drop(['lastTicketingDateTime'], axis=1)
+        df = df.drop(['pricingOptions'], axis=1)
+        df = df.drop(['fees'], axis=1)
+        df = df.drop(['price'], axis=1)
+        # convert itineraries list-like elements to columns
+        df = pd.concat([df.drop(['itineraries'], axis=1), df['itineraries'].apply(pd.Series)], axis=1)
+        df = pd.concat([df.drop([0], axis=1), df[0].apply(pd.Series)], axis=1)
+        ############ to check what columns must be dropped ############
+        # convert df to csv
+        df.to_csv('cheapest_price_trainer_set.csv')
+        print(df)
+        print(df.columns.tolist()) 
+    except ResponseError as error:
+        raise error
+
 # get_flight_offers('MAD', 'ATH', '2022-11-01', 1)
 
 # price_metrics_itinerary('CDG', 'BCN', '2023-11-13')
 
-# cheapest_date('MAD', 'BCN')
+# cheapest_date('SYD', 'BKK')
+get_cheapest_price('CDG', 'MAD')
 
 # print(airport_routes('MAD'))
--- graph.py
+++ graph.py
@@ -33,6 +33,8 @@ import api_calls as api
 # plt.show()
 
 # Function working but raises Amadeus error: Departure airport code is not supported
+# Nodes creation working
+# Weight to be trated
 def create_itinerary_graph(orig, dest):
     '''
     Creates a graph with the origin and destination as nodes and the weight of the edge
@@ -42,25 +44,24 @@ def create_itinerary_graph(orig, dest):
     list_orig = api.airport_routes(orig)
     list_dest = api.airport_routes(dest)
 
-    nodes = list(set(list_orig + list_dest))
+    iterate = list(set(list_orig + list_dest))
+    nodes = iterate
 
+    print("Generating nodes...")
+    for i in iterate:
+        try:
+            aux = api.airport_routes(i)
+            # add aux elements to nodes only if it is not already in nodes
+            nodes = list(set(nodes + aux))
+            # make a pause of 100ms
+            time.sleep(0.1)
+        except:
+            continue
+
+    print("Adding nodes to the graph...")
+    G = nx.Graph()
+    G.add_nodes_from(nodes)
     # print("nodes : ",nodes)
-
-    for i in nodes:
-        print("i : ",i)
-        print("typeof i : ", type(str(i)))
-        print("iata : ", type('ACE'))
-
-        aux = api.airport_routes(i)
-        # add aux elements to nodes only if it is not already in nodes
-        nodes = list(set(nodes + aux))
-        # make a pause of 100ms
-        time.sleep(0.1)
-
-    print(nodes)
-    # G = nx.Graph()
-    # G.add_nodes_from()
-
-    # return G
+    print(len(nodes))
 
 create_itinerary_graph('MAD', 'BCN')
